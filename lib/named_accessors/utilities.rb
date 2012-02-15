7bb852e554f288117db9aca417d5ab5ccbc4388a
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-15 17:00:07 +0100

    create NamedAccessors::Utilities module

---
+++ lib/named_accessors/utilities.rb
@@ -0,0 +1,16 @@
+module NamedAccessors
+  module Utilities
+    private
+    def getter_name(options)
+      extract_as_option(options)
+    end
+
+    def setter_name(options)
+      :"#{extract_as_option(options)}="
+    end
+
+    def extract_as_option(options)
+      options.fetch(:as) rescue raise "You must specify `as` option"
+    end
+  end
+end
