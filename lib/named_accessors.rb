ec1adf95b0e7c1e43421218d2628820c3cbc6033
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-15 17:00:51 +0100

    refactor NamedAccessors module with new modules

--- lib/named_accessors.rb
+++ lib/named_accessors.rb
@@ -1,41 +1,12 @@
-require "named_accessors/version"
+%w{version utilities named_reader named_writer named_accessor}.each do |filename|
+  require "named_accessors/#{filename}"
+end
 
 module NamedAccessors
-  def named_reader(name, options={})
-    method_name, variable_name = getter_name(options), name
-
-    define_method method_name do
-      instance_variable_get :"@#{variable_name}"
-    end
-  end
-
-  def named_writer(name, options={})
-    method_name, variable_name = setter_name(options), name
-
-    define_method method_name do |val|
-      instance_variable_set :"@#{variable_name}", val
-    end
-  end
-
-  def named_accessor(name, options={})
-    named_writer(name, options)
-    named_reader(name, options)
-  end
-
-
-private
-
-  def getter_name(options)
-    extract_as_option(options)
-  end
-
-  def setter_name(options)
-    :"#{extract_as_option(options)}="
-  end
-
-  def extract_as_option(options)
-    options.fetch(:as) rescue raise "You must specify `as` option"
-  end
+  include Utilities
+  include NamedReader
+  include NamedWriter
+  include NamedAccessor
 end
 
 class Object
